1c5ca5ba372c9bc9a86e78428dd45c780702ba74
Haßlauer, Jonas <jonas.hasslauer@student.fhws.de>
2024-01-30 13:49:19 +0100

    Reorganized file structure

--- migrations/2_ImpactCoin_migration.js
+++ migrations/2_ImpactCoin_migration.js
@@ -1,5 +1,5 @@
 const ImpactCoin = artifacts.require("contracts/ImpactCoin.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(ImpactCoin, 1000 * 10 ** 18, "ImpactCoin", 18, "IC");
+    deployer.deploy(ImpactCoin, 1000000000000000, "ImpactCoin", 18, "IC");
 };
